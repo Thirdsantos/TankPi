c3dc6430117e33e577be918988b96a18e8bfb2e8
RKM-Eno-KOJOU <kristoffmalagkit@Gmail.com>
2025-08-26 14:07:22 +0800

    Add files via upload

--- app/routes/sensors.py
+++ app/routes/sensors.py
@@ -6,11 +6,11 @@ def send_sensor_realtime():
 
   sensor = {"ph" : 1, "temperature" : 1, "turbidity" : 1}
   response = requests.post(url,json = sensor)
-  print(f"Recieved: {response.status_code}")
+  print(response.status_code)
 
 def send_sensor_hourly():
   url = f"https://aquacare-5cyr.onrender.com/{aquarium}/hourly_log"
  
   sensor = {"ph" : 1, "temperature" : 1, "turbidity" : 1}
   response = requests.post(url, json = sensor)
-  print(f"Recieved: {response.status_code}")
+  print(response.status_code)
