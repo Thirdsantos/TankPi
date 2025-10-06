6497783c3ced9e6618ac36ce1b6fd763c8b2c7ce
RKM-Eno-KOJOU <kristoffmalagkit@Gmail.com>
2025-10-06 17:54:29 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import uvicorn
 
+# keep aquarium here
 aquarium = 1
 
-
 if __name__ == "__main__":
-  uvicorn.run("app.main:app", port = 8443)
-
+    # pass aquarium into the app via uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=5000, reload=True)
